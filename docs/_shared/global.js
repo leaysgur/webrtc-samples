c8bf02af7a3b549102032fdf4a33ec40a5e7bbee
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-07-19 17:06:31 +0900

    Test signaling

--- docs/_shared/global.js
+++ docs/_shared/global.js
@@ -3,4 +3,29 @@ export const pageId = location.pathname
   .filter(Boolean)
   .pop();
 
+export const query = new URLSearchParams(location.search);
+
 export const signalingKey = localStorage.getItem("SIGNALING_KEY");
+
+export function assert(cond, note) {
+  if (cond) {
+    console.log(`🆗: ${note}`);
+  } else {
+    console.error(`🆖: ${note}`);
+
+    if (query.has("assert")) {
+      alert(`🆖: ${note}`);
+    }
+  }
+}
+
+export function $(query) {
+  const $els = document.querySelectorAll(query);
+  if ($els.length === 0) {
+    throw new Error(`${query} not found!`);
+  }
+  if ($els.length === 1) {
+    return $els[0];
+  }
+  return $els;
+}
--- docs/signaling/bundle.js
+++ docs/signaling/bundle.js
@@ -1,13 +1,55 @@
 const { createChannel } = window.External;
-import { pageId, signalingKey } from "../_shared/global.js";
+import { $, pageId, signalingKey, assert } from "../_shared/global.js";
 
 (async () => {
-  const ch = await createChannel(signalingKey, pageId);
+  assert(typeof signalingKey === "string", "signalingKey exists");
+  assert(typeof pageId === "string", "pageId exists");
 
-  ["@userJoin", "@userLeave", "@message"].forEach(ev => {
-    ch.on(ev, d => console.warn(ev, d));
-  });
-  ch.send("Hello!");
+  let ch;
+  let timer;
 
-  window.ch = ch;
+  $("#create").onclick = async () => {
+    ch = window.ch = await createChannel(signalingKey, pageId);
+    assert(true, `channel "${pageId}" created`);
+
+    ch.on("@message", ({ data }) => {
+      if (data === "ping") {
+        ch.send("pong");
+      }
+      if (data === "pong") {
+        assert(true, "recv pong!");
+      }
+    });
+
+    timer = setInterval(async () => {
+      const users = await ch.fetchUsers();
+      assert(Array.isArray(users), "fetch userId array");
+
+      const $users = $("#users");
+      $users.innerHTML = "";
+
+      for (const user of users) {
+        if (user === ch.id) {
+          $users.innerHTML += `- ${user}(Me)\n`;
+        } else {
+          $users.innerHTML += `- ${user}\n`;
+        }
+      }
+    }, 2000);
+  };
+
+  $("#ping").onclick = () => {
+    if (!ch) return;
+
+    ch.send("ping");
+    assert(true, "ping sent!");
+  };
+
+  $("#close").onclick = () => {
+    ch.close();
+    clearInterval(timer);
+    ch = timer = null;
+
+    assert(true, "channel close");
+  };
 })();
