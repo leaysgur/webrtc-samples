ca401d2b6da117a87911140daa1fb04f088e4e3f
Yuji Sugiura <y.sugiura.0316@gmail.com>
2019-07-19 18:44:37 +0900

    Test enhanced-datachannel

--- docs/enhanced-datachannel/main.js
+++ docs/enhanced-datachannel/main.js
@@ -3,39 +3,55 @@ import { $, pageId, signalingKey } from "../_shared/global";
 const { createChannel } = window.External;
 
 (async () => {
-  let ch;
   $("#connect").onclick = async () => {
-    ch = window.ch = await createChannel(signalingKey, pageId);
+    const ch = await createChannel(signalingKey, pageId);
 
     const users = await ch.fetchUsers();
     console.log(users);
 
     const role = users.length === 1 ? "answer" : "offer";
-    console.log(`You are ${role} side`);
+    if (role === "offer") console.log("Create and send offer...");
+    if (role === "answer") console.log("Wait for offer...");
 
-    const pc = new RTCPeerConnection();
-    const dc = pc.createDataChannel(pageId, { negotiated: true, id: 1 });
+    const pcConf = {
+      bundlePolicy: "max-bundle",
+      iceServers: [{ urls: "stun:stun.l.google.com:19302" }]
+    };
+    const pc = (window.pc = new RTCPeerConnection(pcConf));
+    console.log("pc created w/", pcConf);
+
+    const dcInit = { negotiated: true, id: 1 };
+    const dc = (window.dc = pc.createDataChannel(pageId, dcInit));
+    console.log("dc created w/", dcInit);
 
     pc.onicecandidate = ev => {
+      console.warn("onicecandidate");
       if (ev.candidate === null) return;
       if (ev.candidate.candidate === "") return;
+
       ch.send(ev.candidate);
+      console.log("send icecandidate", ev.candidate);
     };
 
     ch.on("@message", async ({ data }) => {
       if (data.type === "offer") {
+        console.log("receive offer");
+        console.warn(data.sdp);
         await Promise.all([
           pc.setRemoteDescription(data),
           pc.createAnswer().then(answer => pc.setLocalDescription(answer))
         ]);
+        console.log("send answer");
         ch.send(pc.localDescription);
+        console.warn(pc.localDescription.sdp);
       }
-
       if (data.type === "answer") {
+        console.log("receive answer");
+        console.warn(data.sdp);
         await pc.setRemoteDescription(data);
       }
-
       if (data.candidate) {
+        console.log("receive candidate");
         await pc.addIceCandidate(data);
       }
     });
@@ -43,9 +59,22 @@ const { createChannel } = window.External;
     if (role === "offer") {
       await pc.createOffer().then(offer => pc.setLocalDescription(offer));
       ch.send(pc.localDescription);
+      console.log("send offer");
+      console.warn(pc.localDescription.sdp);
     }
 
-    const pdc = (window.pdc = promised(dc));
-    pdc.on("open", () => console.warn("OPEN"));
+    dc.onopen = () => {
+      console.warn("dc.onopen");
+
+      const pdc = (window.pdc = promised(dc));
+      pdc.on("message", (data, accept) => {
+        accept(navigator.userAgent);
+      });
+
+      $("#ask").onclick = async () => {
+        const res = await pdc.send("What is your UA?");
+        console.log(res);
+      };
+    };
   };
 })();
